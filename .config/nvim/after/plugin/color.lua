ac5dbe7b30981790e39845620170e7a031d4c62f
Ryan Denoux <ryanpdenoux@gmail.com>
2023-07-09 12:50:41 +0200

    Move neovim config to lua
    Signed-off-by: Ryan Denoux <ryanpdenoux@gmail.com>

--- .config/nvim/after/plugin/color.lua
+++
@@ -1,4 +0,0 @@
--- vim.cmd("highlight Normal ctermbg=None")
-vim.g.solarized_termtrans = 1
-
-vim.cmd("colorscheme solarized")
---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,29 @@
+require('gruvbox').setup({
+    disable_background = true,
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = true,
+        comments = true,
+        operators = false,
+        folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_indent_guides = false,
+    inverse = true,
+    dim_inactive = false,
+    transparent_mode = true,
+})
+
+function ColorThis(color)
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorThis()
